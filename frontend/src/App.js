91543b203fcca5a8b3fe5321ba88630987cf68a2
naveenawin2707@gmail.com <Awin2707>
2026-02-19 16:18:58 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import reg from '../src/Style/Reg.module.css';
+import Register from './Page/Register';
+
+function App() {
+  return (
+    <Register />
+  );
+}
+
+export default App;
---
+++ frontend/src/Page/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import reg from '../Style/Reg.module.css';
+
+function Register() {
+  return (
+    <div className={reg.main}>
+        <div className={reg.card}>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Register
